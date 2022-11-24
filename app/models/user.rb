63e92ae84f6cbe03575dd4734da63a0293a5eb5b
cmolinan <cmolinan@gmail.com>
2022-11-23 23:58:13 -0500

    Create custom methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :posts
+
+  # Method that returns the 3 most recent posts for a given user.
+  def three_recent_posts(id)
+    Post.where(users_id: id).order(created_at: :desc).limit(3)
+  end
 end
