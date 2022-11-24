4d51cbe3c8d83f479e9c946342e5ea22a9007d73
cmolinan <cmolinan@gmail.com>
2022-11-23 19:18:13 -0500

    fix and adapt models and migrate files to insert data

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :post, foreign_key: 'posts_id'
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :post, foreign_key: 'posts_id'
 end
