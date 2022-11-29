142b4e85ed8e9227d74f14b047f61d779c6d580e
cmolinan <cmolinan@gmail.com>
2022-11-29 16:14:02 -0500

    adding controllers tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+RSpec.describe PostsController, type: :request do
+  context '#index' do
+    before(:each) do
+      get '/users/1/posts'
+    end
+    it 'response status is correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+      expect(response).to render_template('index')
+      expect(response).to render_template('posts/index')
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('All posts for User #1')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+  context '#show' do
+    before(:each) do
+      # In table posts exists a post with id:1 and user with id:1
+      get '/users/1/posts/1'
+    end
+    it 'response status is correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+      expect(response).to render_template('show')
+      expect(response).to render_template('posts/show')
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Post #1 of  user #1')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+RSpec.describe UsersController, type: :request do
+  context '#index' do
+    before(:each) do
+      get '/users'
+    end
+    it 'response status is correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+      expect(response).to render_template('index')
+      expect(response).to render_template('users/index')
+    end
+
+    it 'renders the index template at root path' do
+      get root_path
+      expect(response).to render_template(:index)
+      expect(response).to render_template('index')
+      expect(response).to render_template('users/index')
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Show All users:')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+  context '#show' do
+    before(:each) do
+      # In table Users exists many users
+      get '/users/1'
+    end
+    it 'response status is correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+      expect(response).to render_template('show')
+      expect(response).to render_template('users/show')
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('User Info: 1')
     end
   end
 end
