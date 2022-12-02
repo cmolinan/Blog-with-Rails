23432ff73928a450d5b74a523329c6f7991ebf8e
cmolinan <cmolinan@gmail.com>
2022-12-01 22:02:20 -0500

    Creation of form for new posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,15 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, foreign_key: 'post_id'
 
+  after_save :update_comments_counter
+
   # method that updates the comments counter for a post (using increment!)
-  def update_comments_counter(post_id)
-    Post.find_by(id: post_id).increment!(:comments_counter)
+  # def update_comments_counter(post_id)
+  #   Post.find_by(id: post_id).increment!(:comments_counter)
+  # end
+
+  def update_comments_counter
+    post.increment!(:commentsCounter)
   end
+
 end
