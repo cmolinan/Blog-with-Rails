306fc7ed1927cc328d90df54860fe356f2fc186c
cmolinan <cmolinan@gmail.com>
2022-12-02 00:01:39 -0500

    adding likes to Posts

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+  def create
+    @post = Post.find_by!(id: params[:post_id])
+    current_user = User.find_by!(id: params[:user_id])
+    like = Like.create(author: current_user, post: @post)
+
+    if like.save      
+      redirect_to user_post_likes_url      
+      like.update_likes_counter
+    else
+      redirect_to user_post_likes_url
+    end
+  end
+end
