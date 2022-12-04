9461a9db98b15928d5542433027379fa622cb74f
cmolinan <cmolinan@gmail.com>
2022-12-04 03:21:44 -0500

    updates and fixes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,12 +8,15 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find_by!(id: params[:post_id])
+    @user = User.find_by!(id: params[:user_id])
     comment = Comment.new(comment_params)
     comment.author = current_user
     comment.post = @post
 
     if comment.save
-      redirect_to user_posts_url, notice: 'Comment created !'
+      # debugger
+      # redirect_to user_posts_url, notice: 'Comment created !'
+      redirect_to user_post_url(@user, @post), notice: 'Comment created !'
     else
       redirect_to new_user_post_comment_url, alert: 'Error: comment not created !'
     end
