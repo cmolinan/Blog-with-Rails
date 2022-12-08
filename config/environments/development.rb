f26b3ed85600acd31659834b18e94aca9b56b455
cmolinan <cmolinan@gmail.com>
2022-12-07 21:19:15 -0500

    Install devise gem and basic configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # set up the default URL options for the Devise mailer
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
