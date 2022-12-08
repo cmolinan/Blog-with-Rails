76b96fd6f4a38436b029506b5f8e3559358a9832
cmolinan <cmolinan@gmail.com>
2022-12-08 16:41:01 -0500

    udpate to routes.db file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'users#index'
+   # root 'users#index'
 
+   devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do 
       resources :comments, only: [:new, :create]
