f36282fba8f955a03c4cb0b322366482c5df50a3
cmolinan <cmolinan@gmail.com>
2022-12-09 00:28:19 -0500

    update of reame file and fixes to linters offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
-class ApplicationController < ActionController::Base  
+class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   before_action :authenticate_user!
-  
+
   before_action :update_allowed_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource_or_scope)
@@ -19,6 +19,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
-
 end
-
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,9 @@ class LikesController < ApplicationController
   def create
     @post = Post.find_by!(id: params[:post_id])
     # current_user = User.find_by!(id: params[:user_id])
-    
+
     like = @post.likes.new(author: current_user, post: @post)
     redirect_to user_post_likes_url, notice: 'Like created !'
     return unless like.save
-
-    
   end
 end
