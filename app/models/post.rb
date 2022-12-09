2be8b50ea0209532a7891c58cca3a072676ccdee
cmolinan <cmolinan@gmail.com>
2022-12-09 18:56:09 -0500

    add decrement methods for counters in post and comments models

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,13 +10,18 @@ class Post < ApplicationRecord
   # LikesCounter must be an integer greater than or equal to zero.
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_save :update_post_counter
-  # after_destroy :decrement !!! update_posts_counter
+  after_save :update_posts_counter
+  after_destroy :decrement_posts_counter
 
   def update_post_counter
     author.increment!(:posts_counter)
   end
 
+  # method that decrements the posts counter when a post it's deleted
+  def decrement_posts_counter
+    author.decrement!(:posts_counter)
+  end
+  
   # method which returns the 5 most recent comments for a given post.
   def five_recent_comments(_post_num)
     comments.order(updated_at: :desc).limit(5)
